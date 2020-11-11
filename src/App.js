74715dcf0009a1ae9c7f4050236a5344cbaebebb
mango-jam-0513 <mangojamstar0513@outlook.com>
2020-11-11 20:36:29 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import './App.css';
 import items from './data';
+import Menu from './components/Menu';
+import Categories from './components/Categories'
 // import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 // import Home from './components/Home';
 // import About from './components/About';
@@ -11,6 +13,14 @@ import items from './data';
 // import FunctionClick from "./components/FunctionClick";
 
 function App() {
+
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    const newItems = items.filter((item)=> item.category === category);
+    setMenuItems(newItems);
+  }
   return (
     // <div className="App">
     //   {/* <FunctionClick /> */}
@@ -24,9 +34,17 @@ function App() {
     //     <Route path="/about" exact component={About} />
     //   </Switch>
     // </Router>
-    <div>
 
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
